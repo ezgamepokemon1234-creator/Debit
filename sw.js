c1a9f0186d841c78e851889ef79740a748a78997
ezgamepokemon1234-creator <ezgamepokemon1234@gmail.com>
2026-03-09 18:47:58 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,20 @@
-const CACHE = 'debit-v1';
-const FILES = ['/', '/index.html'];
+const CACHE = 'debit-v2';
+const FILES = [
+  './',
+  './index.html',
+  './pwa-calculator.html',
+  './manifest.json',
+  './icon_1024x1024.png'
+];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(FILES))
+  );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
 });
